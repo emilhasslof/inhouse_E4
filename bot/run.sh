685b59f5883f79dee02dbfacaa1cd18731a37588
Emil Hassslöf <emil.hasslof@gmail.com>
2026-04-06 17:22:06 +0200

    restructure directories, remove inhouse-buddy

--- bot/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Suppress a harmless protobuf double-registration warning from go-steam internals.
-# See: https://protobuf.dev/reference/go/faq#namespace-conflict
-GOLANG_PROTOBUF_REGISTRATION_CONFLICT=warn go run .
